923b585f9ce9445bce3fc2b840310e8edac17888
moosongsong <50647845+moosongsong@users.noreply.github.com>
2020-06-02 20:43:52 +0900

    update hash map

--- HaxhMap/library_HashMap.c
+++ HaxhMap/library_HashMap.c
@@ -1,4 +1,61 @@
-//
-// Created by songe on 2020-06-02.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include "library_HashMap.h"
 
+typedef struct Node{
+    const char * key;
+    const char * value;
+    struct Node * next;
+}Node;
+
+typedef struct HashMap{
+    Node ** buckets;
+    size_t bucketSize;
+    size_t count;
+    FreeFunction freeFunction;
+    DisplayFunction displayFunction;
+    CompareFunction compareFunction;
+}HashMap;
+
+HashMap * initailizeHashMap(FreeFunction freeFunction, DisplayFunction displayFunction,
+        CompareFunction compareFunction, size_t bucketSize){
+    if(freeFunction == NULL || displayFunction == NULL || compareFunction == NULL){
+        perror("function is empty");
+        return NULL;
+    }
+
+    if(bucketSize == 0){
+        perror("bucketSize is 0");
+        return NULL;
+    }
+
+    Node ** buckets = calloc(bucketSize, sizeof(Node*));
+    if(buckets == NULL){
+        perror("bucket allocation is failed");
+        return NULL;
+    }
+
+    HashMap * hashMap = calloc(1, sizeof(HashMap));
+    if(hashMap == NULL){
+        free(buckets);
+        perror("map allocation is failed");
+        return NULL;
+    }
+
+    hashMap->buckets=buckets;
+    hashMap->bucketSize=bucketSize;
+    hashMap->compareFunction=compareFunction;
+    hashMap->displayFunction=displayFunction;
+    hashMap->freeFunction=freeFunction;
+//    hashMap->count=0;
+    return hashMap;
+}
+
+errno_t finalizeHashMap(HashMap * hashMap){
+    if(hashMap == NULL){
+        perror("argument is null");
+        return -1;
+    }
+
+
+}
--- HaxhMap/library_HashMap.h
+++ HaxhMap/library_HashMap.h
@@ -4,5 +4,16 @@
 
 #ifndef MY_LIBRARY_LIBRARY_HASHMAP_H
 #define MY_LIBRARY_LIBRARY_HASHMAP_H
+typedef int errno_t ;
+typedef void(*FreeFunction)(void*);
+typedef char*(*DisplayFunction)(void*);
+typedef int(*CompareFunction)(void*, void*);
+typedef struct HashMap HashMap;
+typedef struct Node Node;
+
+HashMap * initailizeHashMap(FreeFunction freeFunction, DisplayFunction displayFunction,
+                            CompareFunction compareFunction, size_t bucketSize);
+
+
 
 #endif //MY_LIBRARY_LIBRARY_HASHMAP_H
